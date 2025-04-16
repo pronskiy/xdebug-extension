34288e79e5a1c6df11d60bd13733da59bc9633be
Fraser <fraser.chapman@gmail.com>
2025-04-16 21:50:57 +0100

    Fix typo in combined.test.js

--- test/__TEST__/combined.test.js
+++ test/__TEST__/combined.test.js
@@ -6,25 +6,24 @@ const {
 
 
 describe('Combined Tests', () => {
-    test('Should set and retirve cookie with special characters on subequent loads', async () => {
+    test('Should set and retrieve cookie with special characters on subequent loads', async () => {
         // Arrange
         const specialValue = 'TEST=VALUE;WITH!SPECIAL/CHARS?&%';
         const optionsPage = await openOptions();
+        
+        // Act
         await optionsPage.locator('#debugtrigger').fill(specialValue);
         await optionsPage.click('button[type="submit"]');
         await optionsPage.close();
         const page = await openExamplePage();
         const popup = await openPopup();
-
-        // Act
         await popup.click('label[for="debug"]');
-
-        // Assert
         await page.waitForFunction(() => document.cookie);
         await page.reload();
         const popup2 = await openPopup();
-        expect(await popup2.$eval('#debug', (radio) => radio.checked)).toBe(true);
 
+        // Assert
+        expect(await popup2.$eval('#debug', (radio) => radio.checked)).toBe(true);
         await page.close();
     });
 });
